60262124dc84cb71d76545367becbd7c584b9818
DidItWork <ben456jamin7891@gmail.com>
2024-04-19 04:11:14 +0800

    Edited temporal convolutions for EEG data

--- src/contrastive_detector.py
+++ src/contrastive_detector.py
@@ -8,31 +8,25 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class TemporalBlock(nn.Module):
 
-    def __init__(self, in_plane = 3,
-                 out_plane = 3,
-                 stride = 1,
+    def __init__(self, inplane = 3,
+                 outplane = 3,
                  dilation = 1,
                  kernel_size = 3,
                  bias = False):
         
-        super(TemporalBlock, self).__init__
+        super(TemporalBlock, self).__init__()
 
-        assert stride > 0
+        assert dilation > 0
 
-        self.conv = nn.Conv1d(in_plane, out_plane, stride=stride, dilation=dilation, kernel_size=kernel_size, bias=bias)
-
-        if stride > 1:
-            self.identity = nn.MaxPool1d(stride, stride=stride)
+        self.conv = nn.Conv1d(inplane, outplane, stride=1, padding="same", dilation=dilation, kernel_size=kernel_size, bias=bias)
         
-        self.norm_layer = nn.BatchNorm1d(out_plane)
+        self.norm_layer = nn.BatchNorm1d(outplane)
         
     def forward(self, x):
 
-        x = self.conv(x) + self.identity(x)
+        x = self.norm_layer(self.conv(x)) + x
 
-        x = self.norm_layer(x)
-
-        x = nn.ReLU(x)
+        x = nn.ReLU()(x)
 
         return x
         
@@ -46,51 +40,56 @@ class ContrastiveDetector(nn.Module):
         model_name = config.get("model_name", "efficientnet_b0")
         num_features = config.get("num_features", 256)
         num_classes = config.get("num_classes", 6)
+        self.use_contrastive = config.get("use_contrastive", False)
 
         self.xf = torch.tensor([0.])
         self.x2f = torch.tensor([0.])
 
         self.kl_loss = nn.KLDivLoss(reduction="batchmean")
 
-        eeg_blocks = 5
+        eeg_blocks = 6
             
         #Strided Convolution
         module_list = nn.ModuleList([
-            nn.Conv1d(19, 128, stride=5, kernel_size=7, bias=False),
+            nn.Conv1d(19, 64, stride=5, kernel_size=7, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Conv1d(64, 128, kernel_size=3, bias=False),
             nn.BatchNorm1d(128),
             nn.ReLU(),
         ])
 
         for i in range(eeg_blocks):
 
-            module_list.append(TemporalBlock(128, 128, 3, 3, False))
+            module_list.append(TemporalBlock(inplane=128, outplane=128, dilation=3**i, kernel_size=3, bias=False))
         
-        module_list.append(
-            nn.Conv1d(128,num_features,stride=3,kernel_size=3,bias=False),
+        module_list.append(nn.Sequential(
+            nn.Conv1d(128,num_features,dilation=3**eeg_blocks,stride=3**eeg_blocks,kernel_size=3,bias=False),
             nn.BatchNorm1d(num_features),
             nn.ReLU(),
-            )
+            ))
 
         self.backbone_eeg = nn.Sequential(*module_list)
 
         if model_name == "convnext":
 
             self.backbone_spec = convnext_tiny(weights="IMAGENET1K_V1")
-            self.backbone_spec.classifier[2] = nn.Linear(768, num_features)
+            self.backbone_spec.classifier[2] = nn.Linear(768, num_features, bias=False)
         
         elif model_name == "simple":
 
             self.backbone_spec = NaiveCNN()
+            self.backbone_spec.fc2 = nn.Linear(512, num_features, bias=False)
  
         elif model_name == "efficientnet_v2s":
 
             self.backbone_spec = efficientnet_v2_s(weights="IMAGENET1K_V1")
-            self.backbone_spec.classifier[1] = nn.Linear(1280, num_features)
+            self.backbone_spec.classifier[1] = nn.Linear(1280, num_features, bias=False)
             
         else:
 
             self.backbone_spec = efficientnet_b0(weights="IMAGENET1K_V1")
-            self.backbone_spec.classifier[1] = nn.Linear(1280, num_features)
+            self.backbone_spec.classifier[1] = nn.Linear(1280, num_features, bias=False)
         
 #         self.backbone_2d.conv1 = nn.Conv2d(config.get("in_channels",1), 64, kernel_size=(7,7), stride=(2,2), padding=(3,3), bias=False)
         
@@ -98,12 +97,14 @@ class ContrastiveDetector(nn.Module):
 
         self.norm_eeg = nn.BatchNorm1d(num_features)
         self.norm_spec = nn.BatchNorm1d(num_features)
+        self.eeg_fc = nn.Linear(num_features, num_classes, bias=True)
+        self.spec_fc = nn.Linear(num_features, num_classes, bias=True)
 
         self.head = nn.Sequential(
-            nn.Linear(num_features*2, 256, bias=False),
-            nn.BatchNorm1d(256),
+            nn.Linear(num_features*2, num_features, bias=True),
+            nn.BatchNorm1d(num_features),
             nn.ReLU(),
-            nn.Linear(256, num_classes, bias=False),
+            nn.Linear(num_features, num_classes, bias=True),
         )
 
 
@@ -113,11 +114,19 @@ class ContrastiveDetector(nn.Module):
 
     def get_loss(self, predictions, labels):
 
+        eeg_predictions = self.eeg_fc(self.xf)
+        spec_predictions = self.spec_fc(self.x2f)
+
         predictions = F.log_softmax(predictions, dim=1)
+        eeg_predictions = F.log_softmax(eeg_predictions, dim=1)
+        spec_predictions = F.log_softmax(spec_predictions, dim=1)
 
-        kl_loss = self.kl_loss(predictions, labels)
+        kl_loss = self.kl_loss(predictions, labels) + self.kl_loss(eeg_predictions, labels) + self.kl_loss(spec_predictions, labels)
 
-        contrastive_loss = -torch.mean(nn.CosineSimilarity()(self.xf, self.x2f))
+        if self.use_contrastive:
+            contrastive_loss = -torch.mean(nn.CosineSimilarity()(nn.Dropout(p=0.2)(self.xf), nn.Dropout(p=0.2)(self.x2f)))
+        else:
+            contrastive_loss = torch.tensor(0).to(device)
 
         return kl_loss, contrastive_loss
 
@@ -132,6 +141,8 @@ class ContrastiveDetector(nn.Module):
         x = self.backbone_spec(data_dict["spec"])
 
         x2 = self.backbone_eeg(data_dict["eeg"])
+
+        # print(x2.shape)
 
         x2 = nn.Flatten()(x2)
 
@@ -166,4 +177,4 @@ if __name__ == "__main__":
 
     pred = model(data_dict)
 
-    model.get_loss(pred, data_dict["label"])
+    print(model.get_loss(pred, data_dict["label"]))
