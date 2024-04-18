3c6e0f52f858b3d2c9f900b836d4e74821a758da
DidItWork <ben456jamin7891@gmail.com>
2024-04-18 10:08:12 +0800

    Updated src files

--- src/cnn_detector.py
+++ src/cnn_detector.py
@@ -1,53 +1,8 @@
 import torch
 import torch.nn as nn
 from torchvision.models import efficientnet_b0, efficientnet_v2_s, convnext_tiny
+from .networks import NaiveCNN
 import torch.nn.functional as F
-
-class NaiveCNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
-        self.batch_norm1 = nn.BatchNorm2d(32)
-        self.pool1 = nn.MaxPool2d(2, 2)
-
-        nn.init.kaiming_normal_(self.conv1.weight, mode='fan_out', nonlinearity='relu')
-        nn.init.kaiming_uniform_(self.conv1.weight, 
-                               a=0, mode="fan_in", 
-                               nonlinearity="relu") 
-        
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
-        self.batch_norm2 = nn.BatchNorm2d(32)
-        self.pool2 = nn.MaxPool2d(2, 2)
-
-        nn.init.kaiming_uniform_(self.conv2.weight, 
-                               a=0, mode="fan_in", 
-                               nonlinearity="relu") 
-
-        self.avg_pool = nn.AvgPool2d(5, 5)
-        self.flatten = nn.Flatten()
-        
-        self.fc1 = nn.Linear(in_features=8512, out_features=512)
-        self.drop1 = nn.Dropout(p=0.1)
-        
-        self.fc2 = nn.Linear(in_features=512, out_features=6)
-
-        nn.init.kaiming_uniform_(self.fc2.weight, 
-                               a=0, mode="fan_in", 
-                               nonlinearity="relu") 
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.batch_norm1(x)
-        x = self.pool1(x)
-        x = F.relu(self.conv2(x))
-        x = self.batch_norm2(x)
-        x = self.pool2(x)
-        x = self.avg_pool(x)
-        x = self.flatten(x)
-        x = F.relu(self.fc1(x))
-        x = self.drop1(x)
-        x = self.fc2(x)
-        return x
 
 class CNNDetector(nn.Module):
 
@@ -58,6 +13,8 @@ class CNNDetector(nn.Module):
         model_name = config.get("model_name", "efficientnet_b0")
         num_classes = config.get("num_classes", 6)
         use_eeg = config.get("use_eeg", False)
+
+        self.kl_loss = nn.KLDivLoss(reduction="batchmean")
 
         if use_eeg:
             
@@ -87,7 +44,7 @@ class CNNDetector(nn.Module):
         else:
 
             self.backbone_spec = efficientnet_b0(weights="IMAGENET1K_V1")
-            self.backbone_spec.classifier[1] = nn.Linear(1280, num_classes)
+            self.backbone_spec.classifier[1] = nn.Linear(1408, num_classes)
         
 #         self.backbone_2d.conv1 = nn.Conv2d(config.get("in_channels",1), 64, kernel_size=(7,7), stride=(2,2), padding=(3,3), bias=False)
         
--- src/contrastive_detector.py
+++ src/contrastive_detector.py
@@ -1,55 +1,11 @@
 import torch
 import torch.nn as nn
 from torchvision.models import efficientnet_b0, efficientnet_v2_s, convnext_tiny
+from .networks import NaiveCNN
 import torch.nn.functional as F
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-class NaiveCNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
-        self.batch_norm1 = nn.BatchNorm2d(32)
-        self.pool1 = nn.MaxPool2d(2, 2)
-
-        nn.init.kaiming_normal_(self.conv1.weight, mode='fan_out', nonlinearity='relu')
-        nn.init.kaiming_uniform_(self.conv1.weight, 
-                               a=0, mode="fan_in", 
-                               nonlinearity="relu") 
-        
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
-        self.batch_norm2 = nn.BatchNorm2d(32)
-        self.pool2 = nn.MaxPool2d(2, 2)
-
-        nn.init.kaiming_uniform_(self.conv2.weight, 
-                               a=0, mode="fan_in", 
-                               nonlinearity="relu") 
-
-        self.avg_pool = nn.AvgPool2d(5, 5)
-        self.flatten = nn.Flatten()
-        
-        self.fc1 = nn.Linear(in_features=8512, out_features=512)
-        self.drop1 = nn.Dropout(p=0.1)
-        
-        self.fc2 = nn.Linear(in_features=512, out_features=6)
-
-        nn.init.kaiming_uniform_(self.fc2.weight, 
-                               a=0, mode="fan_in", 
-                               nonlinearity="relu") 
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.batch_norm1(x)
-        x = self.pool1(x)
-        x = F.relu(self.conv2(x))
-        x = self.batch_norm2(x)
-        x = self.pool2(x)
-        x = self.avg_pool(x)
-        x = self.flatten(x)
-        x = F.relu(self.fc1(x))
-        x = self.drop1(x)
-        x = self.fc2(x)
-        return x
 
 class ContrastiveDetector(nn.Module):
 
